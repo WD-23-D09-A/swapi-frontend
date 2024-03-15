757c1b616e0ba47d88185100dbb19619adbec7e5
Baris <barisbalcimusic@gmail.com>
2024-03-15 10:36:15 +0100

    function used

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 const body = document.querySelector("body");
-const header = document.createElement("header");
-body.appendChild(header);
-const h2 = document.createElement("h2");
-header.appendChild(h2);
-h2.textContent = "Hello";
+const header = () => {
+  const _headerElement = document.createElement("header");
+  const h2 = document.createElement("h2");
+  _headerElement.appendChild(h2);
+  h2.textContent = "Hello";
+  return _headerElement;
+};
+
+body.appendChild(header());
+
+const button = (text) => {
+  const _button = document.createElement("button");
+  _button.textContent = text;
+  return _button;
+};
+
+const buttons = ["button 1", "button 2", "button 3"];
+
+buttons.forEach((_button) => {
+  body.appendChild(button(_button));
+});
