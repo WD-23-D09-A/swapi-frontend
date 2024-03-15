fbca027356bd1ada935e60cc1829026cf82a14f9
Baris <barisbalcimusic@gmail.com>
2024-03-15 10:13:40 +0100

    header etc added

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const body = document.querySelector("body");
+const header = document.createElement("header");
+body.appendChild(header);
+const h2 = document.createElement("h2");
+header.appendChild(h2);
+h2.textContent = "Hello";
